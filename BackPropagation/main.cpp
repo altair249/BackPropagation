a1eb0fef3ad59227b799126a94c9d3de25ff4a53
Valentin <milhen08@gmail.com>
2017-05-13 19:07:07 +0300

    Update main.cpp

--- BackPropagation/main.cpp
+++ BackPropagation/main.cpp
@@ -237,8 +237,6 @@ int main()
 	out res;
 
 	bool exit = 0;			// Завершения работы программы, когда exit == 1
-	do
-	{
 		cout << "Выберите часть лабораторной 3:";
 		cout << "\n0 - выход;";
 		cout << "\n1 - запустить обучение;\n";
@@ -248,18 +246,16 @@ int main()
 		switch (userAns)
 		{
 		case 0:
-			cout << "\nGoobay!";
+			cout << "\nGoodbay!\n";
 			exit = 1;				// Выход из программы
 			break;
 		case 1:
 			cout << res.fullInfoTeaching();
 			break;
 		default:
-			cout << "\nПовторите ввод.";// Что-то не то
+			cout << "\nОшибка ввода. Повторите запуск программы\n";// Что-то не то
 			break;
 		}
-	} while (!exit);
-
-
+	system("pause");
 	return 0;
 }
